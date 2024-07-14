21dfddaa6a988b214842114e8a38551f3c463e85
Julian O'Grady <julianogrady07@gmail.com>
2024-07-13 21:56:17 -0400

    neutralino working with ts

---
+++ desktop/resources/assets/web-DI-iSA42.js
@@ -0,0 +1,1 @@
+import{W as n}from"./index-BkFt7f24.js";class r extends n{async enable(e){}async disable(e){}}export{r as SafeAreaWeb};
--- src/types/windowExtension.d.ts
+++
@@ -1,5 +0,0 @@
-declare global {
-    interface Window {
-        Neutralino: Neutralino; // Replace `any` with the appropriate type if you know it
-    }
-}
